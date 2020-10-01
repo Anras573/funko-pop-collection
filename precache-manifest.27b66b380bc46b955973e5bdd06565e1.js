5506b696046b8a17435a1014615aa04073c13c5a
Anders Bo Rasmussen <anras573@gmail.com>
2020-10-01 14:41:06 +0300

    Updates

---
+++ precache-manifest.27b66b380bc46b955973e5bdd06565e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567eb8f03189c8c27e6181956de67356",
+    "url": "/funko-pop-collection/index.html"
+  },
+  {
+    "revision": "5c1b02e462d978e23fac",
+    "url": "/funko-pop-collection/static/css/main.a95a05af.chunk.css"
+  },
+  {
+    "revision": "b38882856a3a0569f4b7",
+    "url": "/funko-pop-collection/static/js/2.c88418dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funko-pop-collection/static/js/2.c88418dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1b02e462d978e23fac",
+    "url": "/funko-pop-collection/static/js/main.bb111543.chunk.js"
+  },
+  {
+    "revision": "0c9dc0cac088d007dd4a",
+    "url": "/funko-pop-collection/static/js/runtime-main.7b6c5b51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/funko-pop-collection/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/funko-pop-collection/precache-manifest.bccf1b4aa0d253d0591bb6635cb4c528.js"
+  "/funko-pop-collection/precache-manifest.27b66b380bc46b955973e5bdd06565e1.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/funko-pop-collection/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/funko-pop-collection/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
